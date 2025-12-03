74502911c395c1ddca96761d1c8b2a14cd4eba1a
Lena Yaroshenko <lena@lighstromo.com>
2025-12-03 15:33:23 +1300

    Removed a space...

--- content.js
+++ content.js
@@ -48,7 +48,7 @@ function attachListener(chatInput) {
       timeStyle: 'medium',
     });
 
-    const timestampText = ` ⌚ [Sent: ${timestamp}]`;
+    const timestampText = `⌚ [Sent: ${timestamp}]`;
 
     console.log('Appending timestamp...');
 
